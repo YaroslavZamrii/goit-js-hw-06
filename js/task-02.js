24fa63775b987143cc6b9f4b3d83d8ec6cec02fb
YaroslavZamrii <119421294+YaroslavZamrii@users.noreply.github.com>
2023-04-17 13:27:51 +0300

    task 3

--- js/task-02.js
+++ js/task-02.js
@@ -9,9 +9,28 @@ const ingredients = [
 
 const ulEl = document.querySelector("#ingredients");
 
-for (const ingredient of ingredients) {
+const listContent = ingredients.map((ingredient) => {
   const liEl = document.createElement("li");
   liEl.textContent = ingredient;
   liEl.classList.add("item");
-  ulEl.appendChild(liEl);
-}
+  return liEl;
+});
+
+ulEl.append(...listContent);
+
+// for (const ingredient of ingredients) {
+//   const liEl = document.createElement("li");
+//   liEl.textContent = ingredient;
+//   liEl.classList.add("item");
+//   ulEl.append(liEl);
+// }
+
+// const listContent = ingredients.reduce((acc, ingredient) => {
+//   let liEl = document.createElement("li");
+//   liEl.textContent = ingredient;
+//   liEl.classList.add("item");
+//   acc.push(liEl);
+//   return acc;
+// }, []);
+
+// ulEl.append(...listContent);
